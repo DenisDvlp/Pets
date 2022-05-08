4e05b03f6db7e300a9747b423272770720a2a722
Denys Petrov <dpetrov@accesssoftek.com>
2022-05-08 23:46:45 +0300

    added a comment with microchip details.

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -10,6 +10,12 @@ static Application app;
 // sizeof(long long) 8
 // sizeof(void*)     2
 
+//CPU ATmega32u4: 16 MGz
+//Loader : Arduino Leonardo
+//Flash Memory : 32 kb (4 kb used by loader)
+//SRAM : 2,5 kb
+//EEPROM : 1 kb
+// 
 // Minimal size of the project:
 // Program size : 3 462 bytes(used 12 % of a 28 672 byte maximum) (0, 57 secs)
 // Minimum Memory Usage : 149 bytes(6 % of a 2560 byte maximum)
