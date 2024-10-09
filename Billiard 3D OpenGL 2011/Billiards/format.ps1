c1411acf233e041bc3c5f18996eee09453705388
Denys Petrov <petrovdenn@gmail.com>
2024-10-08 23:27:08 -0400

    Improve clang-format tooling

--- Billiard 3D OpenGL 2011/Billiards/format.ps1
+++ Billiard 3D OpenGL 2011/Billiards/format.ps1
@@ -3,6 +3,6 @@ $Path = ('src', 'inc')
 $Excludes = ""
 Get-ChildItem -Path $Path -Recurse -Include $Extensions -Exclude $Excludes | ForEach-Object {
     Write-Output "Processing  $($_.FullName)"
-    ./clang-format --style="{BasedOnStyle: llvm, IndentWidth: 4}" -i $_.FullName
+    ./clang-format -i $_.FullName
 }
 Write-Output "Formatting done."
