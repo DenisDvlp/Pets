63a670431c5af557cc207748256ebcc98d73bb8f
Denys Petrov <petrovdenn@gmail.com>
2024-10-16 22:24:04 -0400

    Replace loop with MessageLoop in Application

--- Billiard 3D OpenGL 2011/Billiards/inc/win/Application.hpp
+++ Billiard 3D OpenGL 2011/Billiards/inc/win/Application.hpp
@@ -1,17 +1,23 @@
+#pragma once
+#include "MessageLoop.hpp"
 #include "Window.hpp"
 #include "Windows.hpp"
 #include "graphics/Scene.hpp"
 #include "objects/Square.hpp"
 
+namespace win {
+
 class Application final {
   public:
     Application(HINSTANCE hInstance);
     void run();
-    ~Application();
 
-  protected:
-    void runMessageLoop();
+  private:
+    bool init();
     Window m_window;
-    gl::Scene m_scene;
-    gl::objects::Square m_square;
+    gl::Scene m_scene{};
+    gl::objects::Square m_square{};
+    MessageLoop m_messageLoop;
 };
+
+} // namespace win
--- Billiard 3D OpenGL 2011/Billiards/src/win/Application.cpp
+++ Billiard 3D OpenGL 2011/Billiards/src/win/Application.cpp
@@ -1,28 +1,29 @@
 #include "win/Application.hpp"
 
+namespace win {
+
 Application::Application(HINSTANCE hInstance)
-    : m_window{L"billiard_main_window", L"Billiards by Denys Petrov 2024", hInstance} {}
+    : m_window{L"billiard_main_window", L"Billiards by Denys Petrov 2024", hInstance}, m_messageLoop{[this] {
+          m_scene.draw();
+          m_window.swapBuffers();
+      }} {}
 
 void Application::run() {
-    if (m_window.create()) {
+    if (init()) {
+        m_messageLoop.run();
+    }
+}
+
+bool Application::init() {
+    const bool success{m_window.create()};
+    if (success) {
         m_window.show();
         m_square.position.z() = -3;
         m_scene.addSceneObject(m_square);
         m_scene.init();
         m_scene.resize(500, 500);
-        runMessageLoop();
     }
+    return success;
 }
 
-void Application::runMessageLoop() {
-    MSG message{};
-    // PostQuitMessage(0) makes `GetMessage` return zero result to stop the loop.
-    while (GetMessage(&message, NULL, 0, 0)) {
-        TranslateMessage(&message);
-        DispatchMessage(&message);
-        m_scene.draw();
-        SwapBuffers(m_window.m_handleDeviceContext);
-    }
-}
-
-Application::~Application() {}
+} // namespace win
