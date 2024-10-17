d0c5c4f01f23e17ac2a5422ab4e159bda036b0be
Denys Petrov <petrovdenn@gmail.com>
2024-10-16 22:32:06 -0400

    add ns

--- Billiard 3D OpenGL 2011/Billiards/src/main.cpp
+++ Billiard 3D OpenGL 2011/Billiards/src/main.cpp
@@ -1,7 +1,7 @@
 #include "win/Application.hpp"
 
 int WinMain(HINSTANCE hInstance, HINSTANCE /*hprev*/, LPSTR /*cmdline*/, int /*show*/) {
-    Application app(hInstance);
+    win::Application app(hInstance);
     app.run();
     return 0;
 }
