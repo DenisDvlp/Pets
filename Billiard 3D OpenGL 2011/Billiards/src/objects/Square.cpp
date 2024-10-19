102cb252dc9702283334992b573eb989f17f2dca
Denys Petrov <petrovdenn@gmail.com>
2024-10-19 11:58:23 -0400

    change Square::onDraw

--- Billiard 3D OpenGL 2011/Billiards/src/objects/Square.cpp
+++ Billiard 3D OpenGL 2011/Billiards/src/objects/Square.cpp
@@ -11,33 +11,13 @@ auto ptr{gluNewQuadric()};
 void Square::onInit() {}
 
 void Square::onDraw() const {
-    constexpr Color color{0xFF00AA};
+    constexpr Color color{255, 0, 255};
     color::rgb(color);
     MaterialBlock;
     material::reflection(1.0f, 1.0f);
     material::diffusion(1.0f);
     material::ambient(0.0f);
-    glBegin(GL_QUADS);
-    glVertex3f(0, 0, 0);
-    glVertex3f(1, 0, 0);
-    glVertex3f(1, 1, 0);
-    glVertex3f(0, 1, 0);
-    glEnd();
-
-    glBegin(GL_QUADS);
-    glVertex3f(1, 0, 0);
-    glVertex3f(1, 1, 0);
-    glVertex3f(2, 1, 0);
-    glVertex3f(2, 0, 0);
-    glEnd();
-
-    {
-        // MaterialBlock;
-        // material::reflection(1.0f, 1.0f);
-        // material::diffusion(1.0f);
-        // material::ambient(0.0f);
-        // figure::cylinder(0.7f, 0.2f, 0.6f, 64, 32);
-    }
+    figure::square(1.0f, 10, 10);
 }
 
 } // namespace objects
