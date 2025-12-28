3f86cd2d5226a8d66c2c73c1672e3dab105e9b7c
Denys Petrov <petrovdenn@gmail.com>
2025-12-28 14:23:38 -0500

    Add game objects into Core

--- Arduino/nu_pogodi/src/Core.cpp
+++ Arduino/nu_pogodi/src/Core.cpp
@@ -5,120 +5,108 @@
 #include "Animation.hpp"
 #include "stl.hpp"
 
-template<typename T, int S>
-constexpr int size(T(&)[S]) { return S; }
-
-class Wolf : public Sprite {
-private:
-  PicPos pics[3] = {
-    { pic_wolf2, {0, 0} },
-    { pic_w_lleg, {7, 40} },
-    { pic_w_rleg, {34, 35} },
-  };
-  int numberOfPics() const override { return ::size(pics); }
-  const PicPos& getPic(int i) const override { return pics[i]; }
-  void onDraw(Graphics* g) override
-  {
-    g->drawHLine({ pos.x + 27, pos.y + 40 }, 7);
-    g->drawPicture(pic_w_basket, { pos.x, pos.y + 6 });
-  }
-};
-
-class Barn : public Sprite {
-private:
-  PicPos pics[2] = {
-    { pic_dline, { 11, 11 } },
-    { pic_dline, { 11, 30 } },
-  };
-  int numberOfPics() const override { return ::size(pics); }
-  const PicPos& getPic(int i) const override { return pics[i]; }
-  void onDraw(Graphics* g) override
-  {
-    g->drawHLine({ pos.x + -4, pos.y + 11 }, 16);
-    g->drawHLine({ pos.x + -4, pos.y + 30 }, 16);
-  }
-};
-
-class Chicken : public Sprite, public FrameAnimation {
-private:
-  const Picture pics[2] = {
-    Picture(bmp_chicken, 0, 0, 11, 11),
-    Picture(bmp_chicken, 11, 0, 11, 11),
-  };
-  void onDraw(Graphics* g) override
-  {
-    g->drawPicture(pics[frame], pos);
-  }
+class Wolf : public Position {
+  Picture currentBasket{ pic_basket_up_down };
+  bool basketRight = true;
 public:
-  int frame = 0;
-  Chicken() : FrameAnimation(2, 1000) {}
+  Wolf() {
+    basketLeftUp();
+    currentBasket.width = pic_basket_up_down.width / 2;
+  }
+  void basketLeftDown() {
+    currentBasket.x = currentBasket.width;
+    basketRight = false;
+  }
+  void basketLeftUp() {
+    currentBasket.x = 0;
+    basketRight = false;
+  }
+  void basketRightDown() {
+    currentBasket.x = currentBasket.width;
+    basketRight = true;
+  }
+  void basketRightUp() {
+    currentBasket.x = 0;
+    basketRight = true;
+  }
+  void draw(Graphics* g) {
+    g->drawPicture(pic_wolf_body, *this, basketRight);
+    g->drawPicture(currentBasket, {x - 7 + 30 * basketRight, y + 6}, basketRight);
+  }
 };
 
-class Egg : public Sprite, public FrameAnimation {
-private:
-  const Picture pics[4] = {
-    Picture(bmp_eggs, 0, 0, 7, 7),
-    Picture(bmp_eggs, 7, 0, 7, 7),
-    Picture(bmp_eggs, 14, 0, 7, 7),
-    Picture(bmp_eggs, 21, 0, 7, 7),
-  };
-  void onDraw(Graphics* g) override
-  {
-    g->drawPicture(pics[getCurrentFrame()], pos, true);
-  }
+class Background {
 public:
-  Egg() : FrameAnimation(4, 700) {}
+  void draw(Graphics* g) {
+    g->drawPicture(pic_background, { 0, 0 });
+  }
 };
 
-class EggRolling : public Sprite, public Animation {
-private:
-  Chicken chicken;
-  Egg egg;
-  FrameAnimation fa;
-  ValueAnimation<Position> animPos, animPos2;
-  ValueAnimation<bool> animShow;
-  Animation* anims[4] = {
-    &fa, &animShow, &animPos, &animPos2
-  };
-  SequenceAnimation sa;
+class Egg: public Position, public AnimatedPicture {
+  static const Picture pics[4];
 public:
-  Position pos;
-  EggRolling() :
-    fa(2, 300, 3),
-    animPos(egg.pos, { 10, 5 }, { 30, 15 }, 1500, 1),
-    animPos2(egg.pos, { 30, 15 }, { 30, 60 }, 1000, 1),
-    animShow(egg.show, false, true, 0, 1),
-    sa(anims, ::size(anims), 1) {
-    egg.show = false;
-    egg.pos = { 10, 5 };
-  }
-  void setPosition(Position pos)
-  {
-  }
-  void onStart(milliseconds now) override
-  {
-    sa.start(now);
-    egg.start(now);
-  }
-  void onStop() override
-  {
-    sa.stop();
-    chicken.frame = 0;
-    egg.show = false;
-    egg.stop();
-  }
-  void onUpdate(milliseconds now)
-  {
-    sa.update(now);
-    chicken.frame = fa.getCurrentFrame();
-    egg.update(now);
-  }
-  void onDraw(Graphics* g)
-  {
-    chicken.draw(g);
-    egg.draw(g);
+  Egg() : AnimatedPicture(pics, &pics[0], 800, -1) {}
+  void draw(Graphics* g) {
+    g->drawPicture(*currentPicture, *this);
   }
 };
+
+const Picture Egg::pics[4] = {
+  Picture(bmp_eggs_anim, 0, 0, 7, 7),
+  Picture(bmp_eggs_anim, 7, 0, 7, 7),
+  Picture(bmp_eggs_anim, 14, 0, 7, 7),
+  Picture(bmp_eggs_anim, 21, 0, 7, 7),
+};
+
+//class EggRolling : public Sprite, public Animation {
+//private:
+//  Chicken chicken;
+//  Egg egg;
+//  FrameAnimation fa;
+//  ValueAnimation<Position> animPos, animPos2;
+//  ValueAnimation<bool> animShow;
+//  Animation* anims[4] = {
+//    &fa, &animShow, &animPos, &animPos2
+//  };
+//  SequenceAnimation sa;
+//public:
+//  Position pos;
+//  EggRolling() :
+//    fa(2, 300, 3),
+//    animPos(egg.pos, { 10, 5 }, { 30, 15 }, 1500, 1),
+//    animPos2(egg.pos, { 30, 15 }, { 30, 60 }, 1000, 1),
+//    animShow(egg.show, false, true, 0, 1),
+//    sa(anims, ::size(anims), 1) {
+//    egg.show = false;
+//    egg.pos = { 10, 5 };
+//  }
+//  void setPosition(Position pos)
+//  {
+//  }
+//  void onStart(milliseconds now) override
+//  {
+//    sa.start(now);
+//    egg.start(now);
+//  }
+//  void onStop() override
+//  {
+//    sa.stop();
+//    chicken.frame = 0;
+//    egg.show = false;
+//    egg.stop();
+//  }
+//  void onUpdate(milliseconds now)
+//  {
+//    sa.update(now);
+//    chicken.frame = fa.getCurrentFrame();
+//    egg.update(now);
+//  }
+//  void onDraw(Graphics* g)
+//  {
+//    chicken.draw(g);
+//    egg.draw(g);
+//  }
+//};
 
 Position operator+(const Position& l, const Position& r) {
   return { l.x + r.x, l.y + r.y };
@@ -145,70 +133,61 @@ private:
   }
 };
 
+class Stage {
+public:
+  Background background;
+  Wolf wolf;
+  Egg egg;
+};
+
+
 void Core::init(Controller& c, Graphics& g)
 {
+  stage = new Stage;
   controller = &c;
   graphics = &g;
 
-  // init buttons
+  stage->wolf.y = 8;
+  stage->wolf.x = 38;
+
+  stage->egg.y = 1;
+  stage->egg.x = 1;
+
   c.init({ this, &Core::pressDown });
-}
-void Stage(Graphics* graphics)
-{
-  //static Wolf w;
-  //static Chicken c1, c2;
-  //static Barn b;
-  //static Text text;
-  //static EggRolling egg;
 
-  //milliseconds now = millis();
-
-  //text.text = "Счёт: 123";
-  //text.pos = { 70, 0 };
-  //text.draw(graphics);
-
-  //c2.pos.y = 19;
-  //c2.start(now);
-  //c2.update(now);
-  //c2.draw(graphics);
-
-  //b.draw(graphics);
-
-  //w.pos = { 32, 10 };
-  //w.draw(graphics);
-
-  //egg.update(now);
-  //egg.draw(graphics);
-  static Position start = { 60, 30 };
-  static int R = 200;
-  static int A = 0;
-  float radian = degToRad<float>(A);
-  Position end = { start.x + static_cast<int>(cos(radian) * R), start.y + static_cast<int>(sin(radian) * R) };
-  graphics->drawLine(start, end);
-  graphics->drawLine({ 20,40 }, { 20,20 });
-  graphics->drawCircle({ 60, 30 }, A % 100);
-  graphics->drawCircle({ 60, 30 }, A % 20);
-  graphics->drawCircle({ 60, 30 }, A % 40);
-  A += 1;
-  if (A >= 360) A = 0;
 }
 
 void Core::update()
 {
   graphics->clear();
-  Stage(graphics);
+
+  stage->background.draw(graphics);
+  stage->wolf.draw(graphics);
+  stage->egg.update(millis());
+  stage->egg.draw(graphics);
+
+  controller->update();
 }
 
 void Core::pressDown(uint8_t button)
 {
   switch (button) {
   case Controller::BUTTON_X:
+    stage->wolf.x = 38;
+    stage->wolf.basketLeftUp();
+    stage->egg.start(millis());
     break;
   case Controller::BUTTON_Y:
+    stage->wolf.x = 38;
+    stage->wolf.basketLeftDown();
     break;
   case Controller::BUTTON_A:
+    stage->wolf.x = 45;
+    stage->wolf.basketRightDown();
     break;
   case Controller::BUTTON_B:
+    stage->wolf.x = 45;
+    stage->wolf.basketRightUp();
     break;
   }
 }
--- Arduino/nu_pogodi/src/Core.hpp
+++ Arduino/nu_pogodi/src/Core.hpp
@@ -3,10 +3,13 @@
 #include "Controller.hpp"
 #include "Graphics.hpp"
 
+class Stage;
+
 class Core {
 private:
   Controller* controller;
   Graphics* graphics;
+  Stage* stage;
 public:
   void init(Controller &c, Graphics &g);
   void update();
