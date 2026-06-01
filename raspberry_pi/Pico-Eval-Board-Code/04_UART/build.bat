1215bd7f8c682dd1285bca452a5797c9a49a8822
Denys Petrov <petrovdenn@gmail.com>
2026-06-01 00:19:37 -0400

    Add Pico Eval examples

---
+++ raspberry_pi/Pico-Eval-Board-Code/04_UART/build.bat
@@ -0,0 +1,14 @@
+
+:: PICO_BOARD: *.h in sdk\2.2.0\src\boards\include\boards\
+:: PICO_PLATFORM: *.cmake in sdk\2.2.0\cmake\preload\platforms\
+:: PICO_COMPILER: *.cmake in sdk\2.2.0\cmake\preload\toolchains\
+
+cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release ^
+-DPICO_SDK_PATH="%USERPROFILE%\.pico-sdk\sdk\2.2.0" ^
+-DPICO_TOOLCHAIN_PATH="%USERPROFILE%\.pico-sdk\toolchain\15_2_Rel1" ^
+-Dpicotool_DIR="%USERPROFILE%\.pico-sdk\picotool\2.2.0-a4\picotool" ^
+-DPICO_BOARD=pico2_w ^
+-DPICO_PLATFORM=rp2350-arm-s ^
+-DPICO_COMPILER="pico_arm_cortex_m33_gcc"
+
+cmake --build build
---
+++ raspberry_pi/Pico-Eval-Board-Code/06_SPI/build.bat
@@ -0,0 +1,12 @@
+
+:: PICO_BOARD: *.h in sdk\2.2.0\src\boards\include\boards\
+:: PICO_PLATFORM: (optional, inherited from PICO_BOARD) *.cmake in sdk\2.2.0\cmake\preload\platforms\
+:: PICO_COMPILER: (optional, inherited from PICO_PLATFORM) *.cmake in sdk\2.2.0\cmake\preload\toolchains\
+
+cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release ^
+-DPICO_SDK_PATH="%USERPROFILE%\.pico-sdk\sdk\2.2.0" ^
+-DPICO_TOOLCHAIN_PATH="%USERPROFILE%\.pico-sdk\toolchain\15_2_Rel1" ^
+-Dpicotool_DIR="%USERPROFILE%\.pico-sdk\picotool\2.2.0-a4\picotool" ^
+-DPICO_BOARD=pico2_w
+
+cmake --build build
